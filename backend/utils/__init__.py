77b87ff9aec3c6af42b05f163b84793d6bdb7b25
erikafh13 <127170590+erikafh13@users.noreply.github.com>
2026-04-26 15:33:17 +0700

    Delete backend directory

--- backend/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .gdrive import (
-    get_drive_service,
-    list_files_in_folder,
-    download_file,
-    read_file_as_df,
-    read_produk_file,
-    read_stock_file,
-    upload_file_to_drive,
-)
